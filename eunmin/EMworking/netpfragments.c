e619357ca8f83b6e7527cef1a834bb55e6f6b5a4
gcount85 <100mgml@gmail.com>
2022-12-11 13:31:29 +0900

    netpfragments.c 파일 업데이트

--- eunmin/EMworking/netpfragments.c
+++ eunmin/EMworking/netpfragments.c
@@ -1,8 +1,30 @@
 /* IP address structure */
 
-
 struct in_addr
 {
     uint32_t s_addr; /* Address in network byte order (big-endian) */
 };
+
+#include <sys/socket.h>
+int bind(int sockfd, const struct sockaddr *addr,
+         socklen_t addrlen);
+
+// Returns : 0 if OK, -1 on error
+
+#include <sys/socket.h>
+int connect(int clientfd, const struct sockaddr *addr,
+            socklen_t addrlen);
+// Returns: 0 if OK, -1 on error
+
+#include <sys/types.h>
+#include <sys/socket.h>
+int socket(int domain, int type, int protocol);
+// Returns : nonnegative descriptor if OK, −1 on error
+
+
+#include <sys/socket.h>
+int accept(int listenfd, struct sockaddr *addr, int *addrlen);
+
+// Returns: nonnegative connected descriptor if OK, −1 on error
+
 

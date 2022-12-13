e47e7f04e137d6cb2982df26b1254be2b1d19a99
gcount85 <100mgml@gmail.com>
2022-12-13 16:27:46 +0900

    tiny.c 파일 주석 추가

--- eunmin/tiny/tiny.c
+++ eunmin/tiny/tiny.c
@@ -78,7 +78,7 @@ void doit(int fd)
   }
   read_requesthdrs(&rio);
 
-  /* Parse URI from GET request */
+  /* GET요청으로 들어온 URI에서 파싱 */
   is_static = parse_uri(uri, filename, cgiargs);
   if (stat(filename, &sbuf) < 0)
   {
@@ -152,64 +152,88 @@ void read_requesthdrs(rio_t *rp)
   return;
 }
 
-// HTTP URI를 parsing 하는 함수
+// HTTP URI를 parsing 하는 함수: URI, 파일명, CGI인자
 int parse_uri(char *uri, char *filename, char *cgiargs)
 {
   char *ptr;
 
+  // URI에 "cgi-bin" 문자열이 존재하지 않으면 정적 컨텐츠
   if (!strstr(uri, "cgi-bin"))
-  { /* Static content */
-    strcpy(cgiargs, "");
-    strcpy(filename, ".");
-    strcat(filename, uri);
+  {
+    // 문자열 복사 함수: 복사한 문자열을 붙여넣기 할 주소, 복사할 문자열의 시작 주소
+    strcpy(cgiargs, "");   // CGI 인자 스트링 지움
+    strcpy(filename, "."); // 파일명을 .으로 바꿈
+    strcat(filename, uri); // 파일명과 uri를 합침 -> "./파일명.html" 같은 상대 경로가 됨?
+
+    // 만일 uri의 끝 문자가 '/'로 끝나면
     if (uri[strlen(uri) - 1] == '/')
-      strcat(filename, "home.html");
+      strcat(filename, "home.html"); // 파일명 + home.html(기본파일이름)
     return 1;
   }
+
+  // URI에 "cgi-bin" 문자열이 존재하면 동적 컨텐츠
   else
-  { /* Dynamic content */
-    ptr = index(uri, '?');
+  {
+    /* 모든 CGI 인자 추출하기 */
+    ptr = index(uri, '?'); // URI에서 ?의 위치
+
+    // URI에 ?가 있으면
     if (ptr)
     {
-      strcpy(cgiargs, ptr + 1);
+      strcpy(cgiargs, ptr + 1); // ? 다음에 오는 문자열을 cgiargs에 복사
       *ptr = '\0';
     }
+
+    // URI에 ?가 없으면
     else
-      strcpy(cgiargs, "");
+      strcpy(cgiargs, ""); // cgiargs 공백으로
+    /* 모든 CGI 인자 추출하기 끝 */
+
     strcpy(filename, ".");
     strcat(filename, uri);
     return 0;
   }
 }
 
-// 정적 콘텐츠를 클라이언트에게 serve하는 함수
+// 정적 콘텐츠를 클라이언트에게 serve하는 함수: 파일 식별자, 파일명, 파일크기
 void serve_static(int fd, char *filename, int filesize)
 {
-  int srcfd;
+  int srcfd; // 소스 파일 식별자
   char *srcp, filetype[MAXLINE], buf[MAXBUF];
 
   /* Send response headers to client */
-  get_filetype(filename, filetype);
+  get_filetype(filename, filetype); // 파일 이름의 접미어 검사 → 파일 타입 결정
+
+  // 클라이언트에 응답 line과 응답 헤더를 보냄
   sprintf(buf, "HTTP/1.2OK\r\n");
   sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
   sprintf(buf, "%sConnection: close\r\n", buf);
   sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
   sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
   Rio_writen(fd, buf, strlen(buf));
+  // 클라이언트에 응답 line과 응답 헤더를 보냄 - 끝
+
   printf("Response headers:\n");
-  printf("%s", buf);
+  printf("%s", buf); // 빈 줄로 헤더 종료
 
   /* Send response body to client */
-  srcfd = Open(filename, O_RDONLY, 0);
+  srcfd = Open(filename, O_RDONLY, 0); // read를 위해 file 오픈 + 식별자 얻어옴
+
+  // `mmap` 함수로 요청한 파일 가상메모리 영역으로 매핑
+  // 파일 srcfd의 첫 번째 filesize 바이트 주소 srcp에서 시작하는 private read-only 가상 메모리 영역으로 매핑
   srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+
+  // 파일을 메모리로 매핑한 후, 더이상 식별자 필요 없으니 파일 close (메모리 누수 방지)
   Close(srcfd);
+
+  // `srcp`에서 시작하는 `filesize`를 클라이언트의 연결 식별자로 복사
   Rio_writen(fd, srcp, filesize);
+
+  // 매핑된 가상메모리 주소를 반환 (메모리 누수 방지)
   Munmap(srcp, filesize);
 }
 
-/*
- * get_filetype - Derive file type from filename
- */
+/* get_filetype - 파일명으로부터 파일 타입 추출 */
 void get_filetype(char *filename, char *filetype)
 {
   if (strstr(filename, ".html"))
@@ -224,23 +248,25 @@ void get_filetype(char *filename, char *filetype)
     strcpy(filetype, "text/plain");
 }
 
-// 동적 콘텐츠를 클라이언트에게 serve하는 함수
+// 동적 콘텐츠를 클라이언트에게 serve하는 함수: 파일 식별자, 파일명, CGI인자
 void serve_dynamic(int fd, char *filename, char *cgiargs)
 {
   char buf[MAXLINE], *emptylist[] = {NULL};
 
   /* Return first part of HTTP response */
-  sprintf(buf, "HTTP/1.0 200 OK\r\n");
+  sprintf(buf, "HTTP/1.0 200 OK\r\n"); // 클라이언트에게 성공 메시지 전달
   Rio_writen(fd, buf, strlen(buf));
   sprintf(buf, "Server: Tiny Web Server\r\n");
   Rio_writen(fd, buf, strlen(buf));
 
+  // 새로운 자식 프로세스 포크
   if (Fork() == 0)
-  { /* Child */
+  {
     /* Real server would set all CGI vars here */
+    // 자식 프로세스는 query_string 환경 변수를 요청온 URI의 CGI 인자로 초기화
     setenv("QUERY_STRING", cgiargs, 1);
-    Dup2(fd, STDOUT_FILENO);              /* Redirect stdout to client */
-    Execve(filename, emptylist, environ); /* Run CGI program */
+    Dup2(fd, STDOUT_FILENO);              // 자식 프로세스는 자식의 표준 출력(stdout)을 연결 파일 식별자로 재지정
+    Execve(filename, emptylist, environ); // CGI 프로그램 로드 후 실행
   }
-  Wait(NULL); /* Parent waits for and reaps child */
+  Wait(NULL); // 부모는 자식이 종료되어 정리되는 것을 기다리기 위해 `wait` 함수에서 블록
 }
